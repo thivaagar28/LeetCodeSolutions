09e60eb0e47300b89448cf76fd041062bd4bffe8
Thivaagar Loganathan <94023077+thivaagar28@users.noreply.github.com>
2025-02-23 22:00:47 +0800

    Time: 0 ms (100%), Space: 12.6 MB (13.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -8,9 +8,8 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
         if n != 0:  #there are elements to merge from nums2
-            i = 0
+            i = 0   #choose the 1st element of nums2, m is already pointing to the place where new numnbers should be added
             while i < n:
-                nums1[m] = nums2[i]
-                m += 1
+                nums1[m+i] = nums2[i]
                 i += 1
             nums1.sort()

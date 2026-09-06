ec872780077305a4b6ccec8bd5b7a33d645a783e
Thivaagar Loganathan <94023077+thivaagar28@users.noreply.github.com>
2026-09-06 17:01:15 +0800

    Time: 0 ms (100%), Space: 12.4 MB (54.72%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -5,6 +5,28 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+
+        #2 pointers method
+        i,k=0, 0
+        j=len(nums)-1
+
+        while i <= j:
+            if nums[i] != val:
+                nums[k]=nums[i]
+                k+=1
+            i+=1
+        
+        return k
+
+
+
+
+
+
+
+
+
+        """
         i = 0
         j = len(nums) - 1
 
@@ -14,7 +36,7 @@ class Solution(object):
             else:
                 1
 
-        while i <= j:
+        while i <= j: # not just less because need to check the last element that can be checked also
             if nums[i] == val:  #need to replace with the 2nd pointer
                 #2nd pointer should not be the val
                 while nums[j] == val : #move pointer until 2nd pointer not val
@@ -24,7 +46,8 @@ class Solution(object):
                 nums[i] = nums[j] #replace with 2nd pointer value
                 j -= 1  #reduce after replacing
             if i == j:
-                return i+1
+                return i+1 #if no more elements to traverse already
             i += 1
         
         return i+1
+        """
